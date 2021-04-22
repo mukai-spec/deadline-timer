b4b2602fee463a4fc47b185048124a2e03869e7b
mukai-spec <67949305+mukai-spec@users.noreply.github.com>
2021-04-22 14:17:00 +0900

    コード

--- counter.py
+++ counter.py
@@ -8,7 +8,7 @@ sg.theme('default 1')
 
 dt_now = datetime.datetime.now()
 ###アラームの設定
-wave_obj = sa.WaveObject.from_wave_file("好きなアラーム音")
+wave_obj = sa.WaveObject.from_wave_file("music/Faded .wav")
 ###時間設定[入力]
 layout1 = [
     [sg.Text('あるイベントまでの残り時間をお知らせします',font=('',24),pad=(((10,10),(10,10))))],
@@ -17,8 +17,10 @@ layout1 = [
     sg.InputText(dt_now.year, key='-year-',size=(8,1)),sg.Text('年',font=('',18)),
     sg.Combo([f'{i}' for i in range(1,13)],key='-month-',size=(8,1)),sg.Text('月',font=('',18)),
     sg.InputText(key='-day-',size=(8,1)),sg.Text('日',font=('',18)),
-    sg.InputText(key='-hour-',size=(8,1)),sg.Text('時',font=('',18)),],
-    [sg.Button(image_filename='player_button.png',key='実行ボタン') ,sg.Text('',key='-出力-',size=(30,1))],
+    sg.InputText(key='-hour-',size=(8,1)),sg.Text('時',font=('',18)),
+    sg.InputText("0",key='-minutes-',size=(8,1)),sg.Text('分',font=('',18)),
+    ],
+    [sg.Button(image_filename='images/player_button.png',key='実行ボタン') ,sg.Text('',key='-出力-',size=(30,1))],
 ]
 window1 = sg.Window('修論タイマー', layout1)
 while True:
@@ -31,7 +33,8 @@ while True:
             month=int(values['-month-'])
             day=int(values['-day-'])
             hour=int(values['-hour-'])
-            date = datetime.datetime(year=year,month=month,day=day, hour=hour)
+            minute=int(values['-minutes-'])
+            date = datetime.datetime(year=year,month=month,day=day, hour=hour ,minute=minute)
             break
         except:
             window1['-出力-'].update('正しく入力できてません。',font=('',18))
@@ -42,7 +45,7 @@ layout2 = [
             [sg.Text(values['-event-']+"締め切りまで・・・", key='-output-',font=('',50))],
             [sg.Text(size=(25,1), key='-clock-',enable_events=True ,font=("Helvetica", 60),text_color='red')],
             [sg.Text("です。", key='desu',font=('',50))],
-            [sg.Image(filename='ganbarou.png',key=f'{i}') for i in range(1,10)]
+            [sg.Image(filename='images/ganbarou.png',key=f'{i}') for i in range(1,10)]
           ]
 # ウィンドウを作成する
 window2 = sg.Window('修論タイマー', layout2,alpha_channel=0.9)
